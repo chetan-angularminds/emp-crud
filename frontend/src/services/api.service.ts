fc63fd0311a72090c55bb9c6718c708e1ca19dd3
chetan-angularminds <chetan@angularminds.in>
2025-02-25 18:45:45 +0530

    added render backend server url

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HOST_URL = "https://emp-crud-6c99.onrender.com";
+const HOST_URL = "https://emp-crud-6c99.onrender.com/api/";
 
 const api = axios.create({
   baseURL: HOST_URL,
